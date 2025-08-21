133488fa869c2304ee1ba0944214aab6c8d6e19f
Amy Wu <amywu2903@gmail.com>
2025-08-21 16:22:57 -0500

    trying to make sound sliders reset every time unity play is pressed

--- Assets/scripts/SoundMixerManager.cs
+++ Assets/scripts/SoundMixerManager.cs
@@ -1,25 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio; //manages the sound mixer settings in Unity, allowing for audio adjustments and management
 
 public class SoundMixerManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        audioMixer.SetFloat("masterVolume", 0f); // Set the master volume to 0 at the start
+        /*masterSlider.SetValueWithoutNotify(1f);
+    musicSlider.SetValueWithoutNotify(1f);
+    sfxSlider.SetValueWithoutNotify(1f);
+        Debug.Log(masterSlider);*/
+
+        masterSlider.value = 1f;
+        musicSlider.value = 1f;
+        sfxSlider.value = 1f;
+
+        /*audioMixer.SetFloat("masterVolume", 0f); //Set the master volume to 0 (max) at the start
+        audioMixer.SetFloat("musicVolume", 0f);
+        audioMixer.SetFloat("soundFXVolume", 0f);*/
+
+        SetMasterVolume(1f);
+        SetMusicVolume(1f);
+        SetSoundFXVolume(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    void Awake() //not sure
-    {
-        DontDestroyOnLoad(this.gameObject);
     }
 
     public void SetMasterVolume(float volume)
--- Assets/scripts/VolumeSlider.cs
+++ Assets/scripts/VolumeSlider.cs
@@ -1,34 +1,59 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class VolumeManager : MonoBehaviour
 {
     public Slider masterSlider;
     public Slider musicSlider;
     public Slider sfxSlider;
+    public bool firstLevel1Load;
 
     private const string MasterKey = "MasterVolume";
     private const string MusicKey = "MusicVolume";
     private const string SFXKey = "SFXVolume";
+    private const string FirstTimeKey = "FirstTimeLoad";
 
     public AudioSource musicAudioSource;
     public AudioSource sfxAudioSource;
+   
 
     void Start()
     {
-        //load saved values or default to 1
-        masterSlider.value = PlayerPrefs.GetFloat(MasterKey, 1f);
-        musicSlider.value = PlayerPrefs.GetFloat(MusicKey, 1f);
-        sfxSlider.value = PlayerPrefs.GetFloat(SFXKey, 1f);
+        bool isFirstTime = PlayerPrefs.GetInt(FirstTimeKey, 1) == 1;
 
-        //apply volumes on start
-        UpdateVolumes();
+        if (SceneManager.GetActiveScene().buildIndex != 0 || !isFirstTime) //not level 1 OR first lvl 1 load
+        {
+            //LOAD SAVED VALUES or default to 1
+            masterSlider.value = PlayerPrefs.GetFloat(MasterKey, 1f);
+            musicSlider.value = PlayerPrefs.GetFloat(MusicKey, 1f);
+            sfxSlider.value = PlayerPrefs.GetFloat(SFXKey, 1f);
+
+            //apply volumes on start
+            UpdateVolumes();
+
+        }
+        else //first time loading game
+        {
+            //First time in Level 1: reset all sliders to 100%
+            masterSlider.value = 1f;
+            musicSlider.value = 1f;
+            sfxSlider.value = 1f;
+
+            UpdateVolumes();
+
+            // mark that we already loaded Level 1 once
+            PlayerPrefs.SetInt(FirstTimeKey, 0);
+            PlayerPrefs.Save();
+        }
 
         //listen for slider changes
-        masterSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChanged(); });
+            masterSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChanged(); });
         musicSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChanged(); });
         sfxSlider.onValueChanged.AddListener(delegate { OnSFXVolumeChanged(); });
     }
+
 
     void OnMasterVolumeChanged()
     {
@@ -62,7 +87,7 @@ public class VolumeManager : MonoBehaviour
         if (sfxAudioSource != null)
             sfxAudioSource.volume = master * sfxSlider.value;
 
-        //optionally, you can also set AudioListener.volume for overall sound
+        //set AudioListener.volume for overall sound
         AudioListener.volume = master;
     }
 }
